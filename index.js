76a41e58d48c51b67a23e09a43a4d6d592ac14a7
hajunk05 <hajunk05@gmail.com>
2025-07-22 17:41:39 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+let persons = [
+	{
+		name: 'Arto Hellas',
+		number: '21345231',
+		id: '1',
+	},
+	{
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+		id: '2',
+	},
+	{
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+		id: '3',
+	},
+]
+
+app.get('/api/persons', (request, response) => {
+	response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body
+	const personToAdd = {
+		name: body.name,
+		number: body.number,
+		id: body.id,
+	}
+	persons = persons.concat(personToAdd)
+	response.status(204).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = request.params.id
+	if (!persons.find((person) => person.id === id)) {
+		response.status(404).end()
+	}
+
+	persons = persons.filter((person) => person.id !== id)
+	response.json(persons)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
